718e6039e48753545a76ec462e635c56e762717d
Joey <aileen60222@gmail.com>
2020-04-19 15:39:55 +0800

    update

--- index-jquery.js
+++ index-jquery.js
@@ -1,10 +1,9 @@
 $( document ).ready(function() {    
     $('#addImg').on('click', function(){
         let url = $('#imgUrl').val();
-        $('#imgList').prepend(`<div class="col-6 col-md-4 mt-2 mb-2 imgbox">
+        $('#imgList').prepend(`<div class="col-6 col-md-4 mt-2 mb-2">
                                 <img src='${url}' class="img-thumbnail">
                               </div>`)
     })
 });
-
 
